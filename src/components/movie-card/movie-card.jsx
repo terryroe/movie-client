bc8832dc501ceb055ccd37460ecc81ca5375cdaf
Terry Roe <terryroe@gmail.com>
2023-07-20 14:46:03 -0600

    make site responsive

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,26 @@
 import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-  return <div onClick={() => onMovieClick(movie)}>{movie.Title}</div>;
+  return (
+    <Card onClick={() => onMovieClick(movie)}>
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
   }),
   onMovieClick: PropTypes.func.isRequired,
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const MyFlixApplication = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 // Finds the root of your app
